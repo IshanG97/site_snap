3212082742eeec1b1d835f6d4da3b691f6fb1b00
ishgod01 <ishan.godawatta@arm.com>
2025-01-14 01:19:19 +0000

    Setting up browser paths for testing on various browsers

---
+++ browser_path.py
@@ -0,0 +1,22 @@
+BROWSER_PATH = {
+    'macos': {
+        'safari': "Safari",
+        'chrome': "/Applications/Google Chrome.app",
+        'firefox': "/Applications/Firefox.app",
+        'edge': "/Applications/Microsoft Edge.app",
+        'opera': "/Applications/Opera.app",
+    },
+    'windows': {
+        'chrome': "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+        'chrome-canary': "C:\\Users\\armux\\AppData\\Local\\Google\\Chrome SxS\\Application\\chrome.exe",
+        'firefox': "C:\\Program Files\\Mozilla Firefox\\firefox.exe",
+        'edge': "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
+        'opera': "C:\\Program Files\\Opera\\launcher.exe",
+        'internet_explorer': "C:\\Program Files\\internet explorer\\iexplore.exe",
+    },
+    'linux': {
+        'chrome': "/usr/bin/google-chrome",
+        'firefox': "/usr/bin/firefox",
+        'opera': "/usr/bin/opera",
+    }
+}
